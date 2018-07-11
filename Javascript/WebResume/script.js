7cf61b01978e665478d43089c7e25545403368c1
ravi4all <ravikanttyagi.2295@gmail.com>
2018-07-11 12:02:46 +0530

    Add files via upload

--- Javascript/WebResume/script.js
+++ Javascript/WebResume/script.js
@@ -1,9 +1,14 @@
 var container;
+var charX = 0;
+var moveX = 0;
+var image;
 window.addEventListener("load", function(){
     
     var screenHeight = window.innerHeight;
     container = document.getElementById("container");
     container.style.height = screenHeight+"px";
+
+    image = document.getElementById("image");
 
     document.getElementById("body").addEventListener("keyup",movePlayer);
 });
@@ -12,9 +17,15 @@ function movePlayer(){
     var key = event.keyCode;
     // console.log(key);
     if (key == 39){
-        container.style.transform = 'translateX(-10px)';
+        moveX = -200;
+        charX += moveX;
+        container.style.transform = 'translateX('+charX+'px)';
+        image.style.transform = 'rotateY(0deg)';
     }
     else if(key == 37){ 
-
+        moveX = 200;
+        charX += moveX;
+        container.style.transform = 'translateX('+charX+'px)';
+        image.style.transform = 'rotateY(180deg)';
     }
 }
